ba85f9387cbc6fa8561f11c2acbe710fac5b886c
Dietrich Epp <depp@zdome.net>
2013-07-28 12:44:41 -0700

    Print better error messages from File::read()

--- src/file_posix.cpp
+++ src/file_posix.cpp
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <limits>
 #include <stdexcept>
+#include <string.h>
+#include <errno.h>
 #pragma GCC diagnostic ignored "-Wsign-compare"
 namespace Seven {
 namespace File {
@@ -17,22 +19,32 @@ struct Fdes {
     int fd;
 };
 
+static void error(int err_code, const std::string &path, const char *what) {
+    if (err_code != 0) {
+        std::fprintf(stderr, "%s: %s: %s\n", path.c_str(), what,
+                     strerror(err_code));
+    } else {
+        std::fprintf(stderr, "%s: %s\n", path.c_str(), what);
+    }
+    throw std::runtime_error("File::read()");
+}
+
 Buffer read(const std::string &path) {
     int fdes = ::open(path.c_str(), O_RDONLY);
     if (fdes < 0)
-        throw std::runtime_error("File::read()");
+        error(errno, path, "open()");
     Fdes fdobj(fdes);
 
     struct stat st;
     int r = ::fstat(fdes, &st);
     if (r != 0)
-        throw std::runtime_error("File::read()");
+        error(errno, path, "fstat()");
 
     if (!S_ISREG(st.st_mode))
-        throw std::runtime_error("File::read()");
+        error(0, path, "not a regular file");
 
     if (st.st_size > std::numeric_limits<std::size_t>::max())
-        throw std::runtime_error("File::read()");
+        error(0, path, "file too large");
     std::size_t size = static_cast<std::size_t>(st.st_size);
     Buffer buf(size);
     std::size_t pos = 0;
@@ -43,9 +55,9 @@ Buffer read(const std::string &path) {
             reinterpret_cast<unsigned char *>(buf.data()) + pos,
             size - pos);
         if (amt < 0)
-            throw std::runtime_error("File::read()");
+            error(errno, path, "read()");
         if (amt == 0)
-            throw std::runtime_error("File::read()");
+            error(0, path, "unexpected EOF");
         pos += amt;
     }
 
